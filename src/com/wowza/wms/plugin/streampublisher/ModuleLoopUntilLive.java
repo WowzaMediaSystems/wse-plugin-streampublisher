f867dba941ced706d8df991b91f1c0e5ced6fed7
Dave Nelson <dnelson@wowza.com>
2016-04-14 14:44:22 -0700

    Fixed commentInCode formatting.

--- src/com/wowza/wms/plugin/streampublisher/ModuleLoopUntilLive.java
+++ src/com/wowza/wms/plugin/streampublisher/ModuleLoopUntilLive.java
@@ -1,5 +1,5 @@
-/**
- * This code and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC.  All rights reserved.
+/*
+ * This code and all components (c) Copyright 2006 - 2016, Wowza Media Systems, LLC. All rights reserved.
  * This code is licensed pursuant to the Wowza Public License version 1.0, available at www.wowza.com/legal.
  */
 package com.wowza.wms.plugin.streampublisher;
